ac36dd7ebc0349c688641d9d23b4da4c68368230
김 성 민 <origene42@naver.com>
2022-05-17 14:37:27 +0900

    dbtool

--- django_back/django_back/urls.py
+++ django_back/django_back/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('dj_rest_auth.urls')),
     path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('movies/', include('movies.urls')),
 ]
